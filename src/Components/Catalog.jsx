4f54d49f25170dd44f38dad43e0289c1f33fb420
markopaunovic1 <marpau970514@hotmail.com>
2023-09-29 00:43:21 +0200

    fetcing data from api key

--- src/Components/Catalog.jsx
+++ src/Components/Catalog.jsx
@@ -5,20 +5,35 @@ const URL = 'https://api.jsonbin.io/v3/b/650a7ebece39bb6dce7f5683';
 
 const Catalog = (props) => {
 
-    const [dog, setDog] = useState(0)
+    const [dog, setDog] = useState("");
 
     useEffect(() => {
         fetch(URL)
         .then ((response) => response.json())
-        .then ((data) => console.log(data.record));
+        .then ((data) => {
+                console.log(data.record[0].name);
+                setDog(data.record[0].name);
+
+        });
             
     }, []);
 
+    const viewDog = () => {
+        {dog.map((item)  => {
+            <li key={item.id}>{item.breed}</li>
+            })};
+    }
+
     return (
-        <section>
-            <h1>Catalog page</h1>
-{/*             <p>current item: {setItem(json.current.name)}</p>
- */}    </section>
+        <div>
+            <section>
+                <h1>Registered Dogs:</h1>
+                    <ul>
+                        <p>{dog}</p>
+                    </ul>
+    {/*             <p>current item: {setItem(json.current.name)}</p>
+    */}    </section>
+        </div>
     )
 }
 
