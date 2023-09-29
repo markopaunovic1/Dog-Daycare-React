47a400cedb3b3f3cc34ebecc9de94df5ee4731b4
markopaunovic1 <marpau970514@hotmail.com>
2023-09-29 21:35:40 +0200

    fetching dog images from api

--- src/Components/Catalog.jsx
+++ src/Components/Catalog.jsx
@@ -1,4 +1,4 @@
-import { useState ,useEffect } from "react"
+import { useState ,useEffect, createElement } from "react"
 import { json } from "react-router-dom";
 
 const URL = 'https://api.jsonbin.io/v3/b/650a7ebece39bb6dce7f5683';
@@ -20,17 +20,27 @@ const Catalog = (props) => {
     }, []);
 
     useEffect(( onSelectDog ) => {
-        const elements = dogs.map((dogsData) => (<li key={dogsData.id} onClick={() => onSelectDog(dogsData)} className="dogItem">{dogsData.name} {dogsData.breed}</li> ));
+        const dogPic = document.createElement('img');
+
+        const elements = dogs.map((dogsData) => 
+        (<li key={dogsData.record} onClick={() => onSelectDog(dogsData)} className="dogItem">
+            {dogsData.name + ' '}
+            {dogsData.breed} <img key={dogsData} src={dogsData.img} className="all-dogs-image"></img></li> ));
+            
         setdogElements(elements);
         console.log(elements.dogsData);
     }, [dogs]);
     
-const showDogImage = () => {
+/* const showDogImage = () => {
     const dogImage = dogs.map((dogsData) => (<div>{dogsData.img}</div>))
     console.log(dogImage)
     return(
         <img>{dogImage}</img>
     )
+} */
+
+const selectedDogs = (event, index) => {
+    const dogpic = document.createElement('img');
 }
 
     return (
@@ -39,7 +49,9 @@ const showDogImage = () => {
                 <h1 className="Catalog-title">Registered Dogs:</h1>
                     <ul className="list-of-dogs">
                         <p>{dogElements}</p>
-                        <ul>{showDogImage}</ul>
+                        {/* {dogs.map((image, index) => 
+                        <img key={index} src={image.img} alt={'Image ${index'}></img>)} */}
+                        {/* <ul>{showDogImage}</ul> */}
                     </ul>
     {/*             <p>current item: {setItem(json.current.name)}</p>
     */}    </section>
