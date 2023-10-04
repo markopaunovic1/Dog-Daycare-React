85fe770b5c2cba3bbb929e93c01f868257024dcb
markopaunovic1 <marpau970514@hotmail.com>
2023-10-04 18:07:49 +0200

    successfully getting all data for a specific dog in DogInfo page

--- src/Components/Catalog.jsx
+++ src/Components/Catalog.jsx
@@ -1,49 +1,39 @@
 import { useState ,useEffect, createElement } from "react"
-import { Link, json, useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import noImage from "../assets/noImage.png"
 
 const URL = 'https://api.jsonbin.io/v3/b/650a7ebece39bb6dce7f5683';
 
 const Catalog = (props) => {
 
-    const [dogs, setDog] = useState([]);
+    const [dogs, setDogs] = useState([]);
     const [dogElements, setdogElements] = useState([]);
     const [selectedDog, setSelectedDog] = useState(0);
 
     useEffect(() => {
         fetch(URL)
         .then ((response) => response.json())
-        .then ((data) => {
-                setDog(data.record);
-                console.log(data);
+        .then ((data) => { setDogs(data.record);
+            console.log(data);
         });
 
     }, []);
 
-    useEffect((props) => {
+    useEffect(() => {
         const length = Array.length;
 
         const elements = dogs.map((dogsData) => (
-
-        <Link to={`/DogInfo/ ${ dogsData }`}>
-        <li key={dogsData} className="dogItem">
-            { dogsData.name + ' ' }
-            { dogsData.breed } 
-            <img key={ dogsData } src={ dogsData.img } 
-            onError={(e) => (e.currentTarget.src = noImage)} className="all-dogs-image"></img>
-        </li>
+        <Link to={`/DogInfo/${ dogsData.chipNumber }`}>
+            <li key={dogsData} className="dogItem">
+                { dogsData.name + ' ' }
+                { dogsData.breed + ' ' } 
+                <img key={ dogsData } src={ dogsData.img } 
+                onError={(e) => (e.currentTarget.src = noImage)} className="all-dogs-image"></img>
+            </li>
         </Link> ));
             
         setdogElements(elements);
     }, [dogs]);
-    
-    const passDogData = () => {
-        const dog = {dog};
-
-        return(
-            <Link to={`/dogInfo/${data.name}/ ${data.age}`}> </Link>
-        )
-    }
 
     return (
         <div>
@@ -51,10 +41,7 @@ const Catalog = (props) => {
                     <ul className="list-of-dogs">
                     <h1 className="Catalog-title">Registered Dogs:</h1>
                         <p>{dogElements} elements</p>
-                        {/* {dogs.map((image, index) => 
-                        <img key={index} src={image.img} alt={'Image ${index'} className="dogItem"></img>)} */}
                     </ul>
-    {/* <p>current item: {setItem(json.current.name)}</p */}
             </section>
         </div>
     )
--- src/Components/DogInfo.jsx
+++ src/Components/DogInfo.jsx
@@ -1,26 +1,57 @@
 import { useState } from "react"
 import { useParams } from "react-router-dom"
+import { useEffect } from "react";
+import noImage from "../assets/noImage.png"
+
+const URL = 'https://api.jsonbin.io/v3/b/650a7ebece39bb6dce7f5683';
 
 const DogInfo = () => {
-    const { id } = useParams();
-    console.log(id + "= this is id ");
+    const params = useParams();
+    const currentDog = params.id;
+
+    const [dog, setDog] = useState([]);
+
+    useEffect(() => {
+        fetch(URL)
+        .then ((response) => response.json())
+        .then ((data) => { 
+
+            const specificDog = data.record.find((dog) => dog.chipNumber === currentDog);
+            console.log("current dog: ",specificDog);
+
+            if (specificDog) {
+                setDog(specificDog);
+              } else {
+                // Handle if dog not found
+                console.log('Dog not found');
+              }
+            })
+            .catch((error) => {
+              console.error('Error fetching dog data:', error);
+            });
+        }, []);
 
     return (
         <section className="dog-info-page">
+            {dog && dog.owner && (
             <div className="dog-container">
-                <div className="dog-info-image">
-                    <h1>dog Info - {id}</h1>
-                </div>
-                <p>Image of dog</p>
-                <p>name: name</p>
-                <p>age: age</p>
-                <p>breed: breed</p>
-                <p>sex: sex</p>
-                <p>chipnumber: chipnumber</p>
-                <p>owner: owner</p>
-                <p>phone number: phone number</p>
-                <p>present: present</p>
+                    <div className="dog-info-image">
+                        <h1>dog Info</h1>
+                    </div>
+                    
+                    <img key={ dog } src={ dog.img } 
+                    onError={(e) => (e.currentTarget.src = noImage)} className="all-dogs-image"></img>
+                    <p>This is current dog ID: {params.id}  </p>
+                <p>name: {dog.name}</p>
+                <p>age: age {dog.age}</p>
+                <p>breed: {dog.breed}</p>
+                <p>sex: {dog.sex}</p>
+                <p>chipnumber: {dog.chipNumber}</p>
+                <p>owner: {dog.owner.name}</p>
+                <p>phone number: {dog.owner.lastName}</p>
+                <p>present: {dog.present}</p >
             </div>
+            )}
         </section>
     )
 }
